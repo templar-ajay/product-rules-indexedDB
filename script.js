9f33a54965bd57d16bd6f3a5fcf94d96a11220bc
templar-command0 <templar.command0@gmail.com>
2022-08-23 14:01:50 +0530

    basic crud operations in indexedDB

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function openDB() {
     request.onupgradeneeded = (e) => {
       const db = e.target.result;
       const objectStore = db.createObjectStore("rules", {
-        autoIncrement: true,
+        keyPath: "master",
       });
     };
   });
@@ -37,13 +37,120 @@ async function addData(data) {
     console.log(`Error !! adding data to database ${e.target.error}`);
   };
   request.onsuccess = (e) => {
-    console.log(`${data} successfully added to database`);
+    console.log(data, `successfully added to database`);
   };
 }
 
+// adding data to the database
 const data = {
   master: "apple-watch",
+  id: 1,
   childProducts: ["shoes Nike", "iPhone13proMax", "Race Bike"],
 };
-
+const data2 = {
+  master: "apple-watch2",
+  id: 1,
+  childProducts: ["shoes Nike", "iPhone13proMax", "Race Bike"],
+};
+const data3 = {
+  master: "apple-watch3",
+  id: 1,
+  childProducts: ["shoes Nike", "iPhone13proMax", "Race Bike"],
+};
 addData(data);
+addData(data2);
+addData(data3);
+
+//read data
+
+async function showCards() {
+  if (!db) db = await openDB();
+
+  const objectStore = db
+    .transaction(["rules"], "readonly")
+    .objectStore("rules");
+
+  // const request = objectStore.get("apple-watch");
+  // const request = objectStore.count("apple-watch");
+  // const request = objectStore.getAll();
+  // const request = objectStore.getAll("apple-watch");
+
+  // const request = objectStore.getAllKeys();
+  const request = objectStore.openCursor();
+
+  request.onsuccess = (e) => {
+    const cursor = request.result || e.target.result;
+    if (cursor) {
+      console.log(`Requested data is `, e.target.result);
+
+      cursor.continue();
+    } else {
+      console.log(`all entries displayed`);
+    }
+  };
+
+  // request.onsuccess = (e) => {
+  //   console.log(`Requested data is `, e.target.result);
+  // };
+
+  request.onerror = (e) => {
+    console.log(`Error!! ${e.target.error}`);
+  };
+}
+showCards();
+
+// updating data
+async function updateData(keyPath, key, value) {
+  if (!db) db = await openDB();
+
+  const objectStore = db
+    .transaction(["rules"], "readwrite")
+    .objectStore("rules");
+
+  const request = objectStore.get(keyPath);
+
+  request.onerror = (e) => {
+    console.log(`Error!!! fetching "${keyPath}" from database`, e.target.error);
+  };
+
+  request.onsuccess = (e) => {
+    const data = e.target.result;
+
+    data[key] = value;
+
+    const updateRequest = objectStore.put(data);
+    updateRequest.onerror = (e) => {
+      console.log("Error!!", e.target.error);
+    };
+    updateRequest.onsuccess = (e) => {
+      console.log(`Data updated successfully`, e.target.result);
+    };
+  };
+}
+
+updateData("apple-watch", "id", 1);
+
+// deleting data
+
+async function deleteData(data) {
+  if (!db) db = await openDB();
+  const objectStore = db
+    .transaction(["rules"], "readwrite")
+    .objectStore("rules");
+
+  const request = objectStore.openCursor();
+  request.onerror = (e) => {
+    console.log(`Error while opening cursor in deleteData fn`, e.target.error);
+  };
+  request.onsuccess = (e) => {
+    const cursor = request.result || e.target.result;
+    if (cursor) {
+      console.log(cursor);
+      if (cursor.value.master === data) {
+        cursor.delete();
+      } else cursor.continue();
+    }
+    console.log(`all entries displayed`);
+  };
+}
+deleteData("apple-watch");
